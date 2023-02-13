f620c9201fdaccb3bc96f87491457b86567f6edc
Vincent Amato <vincentaamato@gmail.com>
2023-02-13 16:35:50 -0500

    added csv viewing functionality

--- src/main.ts
+++ src/main.ts
@@ -115,7 +115,7 @@ function parseCommandCall(command: string) {
             break;
         }
         case "view": {
-            console.log("got view command!");
+            csvViewer()
             //something else...
             break;
         }
@@ -144,7 +144,6 @@ function parseCommandCall(command: string) {
     }
 }
 
-function print(command: string) {
 function print(output: string) {
     // As far as TypeScript knows, there may be *many* elements with this class.
     const maybeDivs: HTMLCollectionOf<Element> = document.getElementsByClassName('repl-history')
@@ -212,8 +211,19 @@ function csvLoader(targetPath: String) {
 }
 
 function csvViewer() {
-    console.log("not implemented yet, go yell at connor")
-    //Waiting for a container to throw an html table into...
+    let replHistory = document.getElementsByClassName("repl-history")[0];
+    let table = document.createElement("table")
+    for (let row = 0; row < activeData.length; row++) {
+        let rowElement = table.appendChild(document.createElement("tr"))
+        for (let col = 0; col < activeData[row].length; col++) {
+            let colNode = document.createTextNode(activeData[row][col])
+            let colElement = document.createElement("td")
+            colElement.appendChild(colNode)
+            colElement.className = "repl-command"
+            rowElement.appendChild(colElement)
+        }
+    }
+    replHistory.appendChild(table)
 }
 
 function csvSearcher(targIndex: string, searchTerm: string) {
